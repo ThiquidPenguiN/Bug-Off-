66e08dcbebe10876f3f64de4800aff07ecad872b
ThiquidPenguiN <thiquidpenguin@gmail.com>
2022-01-04 19:10:14 -0800

    added gameover logic
    rearranged code to allow game over/victory conditions

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,13 +4,14 @@ public class PlayerControl : MonoBehaviour
 
 
 {
-    private int health = 10;
-    private int score = 0;
+    private float health = 3.0f;
+    private int score = 10;
     private bool swatterPressed = false;
     private bool isReleased = true;
     private int countFrame = 0;
     private int waitFrames = 29;
     private BoxCollider swatterCollider;
+    private bool gameOver = false;
 
     // Start is called before the first frame update
     void Start()
@@ -28,24 +29,45 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Mouse movement for player
-        float mouseHorizontal = Input.GetAxis("Mouse X");
-        float mouseVertical = Input.GetAxis("Mouse Y");
-        transform.Translate(mouseHorizontal, 0, mouseVertical, Space.World);
-
-        //Swatter controls
-        if (Input.GetKey(KeyCode.Space))
+        //allow control of player if game is active
+        if (!gameOver)
         {
-            SwatterPressed();
+            //Mouse movement for player
+            float mouseHorizontal = Input.GetAxis("Mouse X");
+            float mouseVertical = Input.GetAxis("Mouse Y");
+            transform.Translate(mouseHorizontal, 0, mouseVertical, Space.World);
+
+            //Swatter controls
+            if (Input.GetKey(KeyCode.Space))
+            {
+                SwatterPressed();
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                transform.Rotate(-45, 0, 0);
+                swatterPressed = false;
+                //enable-disable swatter collider to prevent cheese
+                swatterCollider.enabled = true;
+                countFrame = 0;
+            }
         }
 
-        if (Input.GetKeyUp(KeyCode.Space))
+
+        //if health falls below 3, gameover
+        if (health <= 0)
         {
-            transform.Rotate(-45, 0, 0);
-            swatterPressed = false;
-            //enable-disable swatter collider to prevent cheese
-            swatterCollider.enabled = true;
-            countFrame = 0;
+            Debug.Log("Game Over");
+            PauseGame();
+            gameOver = true;
+        }
+
+        //countdown from 100 enemies. @ 0 enemies, game over
+        if (score <= 0)
+        {
+            Debug.Log("Victory!");
+            PauseGame();
+            gameOver = true;
         }
     }
 
@@ -57,7 +79,7 @@ public class PlayerControl : MonoBehaviour
 
     public void Score()
     {
-        score++;
+        score--;
         Debug.Log(score.ToString());
     }
 
@@ -80,5 +102,10 @@ public class PlayerControl : MonoBehaviour
             //enable-disable swatter collider to prevent cheese
             swatterCollider.enabled = false;
         }
+    }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0;
     }
 }
