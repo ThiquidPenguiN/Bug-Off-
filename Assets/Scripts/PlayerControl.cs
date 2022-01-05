ec1871cab806afa272d30878ef1e1b80d52e5494
ThiquidPenguiN <thiquidpenguin@gmail.com>
2022-01-05 10:46:45 -0800

    moved gamedata methods to own script
    confirmed game working after migration of code to new script

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,15 +4,11 @@ public class PlayerControl : MonoBehaviour
 
 
 {
-    public GameObject fullHeartPrefab;
-    public GameObject halfHeartPrefab;
-    private int health = 6; //2 units = 1 full heart
-    private int score = 100;
     private bool swatterPressed = false;
     private int countFrame = 0;
     private int waitFrames = 29;
     private BoxCollider swatterCollider;
-    private bool gameOver = false;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +20,6 @@ public class PlayerControl : MonoBehaviour
         Cursor.visible = false;
         transform.Rotate(-45, 0, 0);
 
-        DrawHearts(health);
 
     }
 
@@ -32,7 +27,7 @@ public class PlayerControl : MonoBehaviour
     void Update()
     {
         //allow control of player if game is active
-        if (!gameOver)
+        if (!GameDataScript.gameOver)
         {
             //Mouse movement for player
             float mouseHorizontal = Input.GetAxis("Mouse X");
@@ -55,26 +50,10 @@ public class PlayerControl : MonoBehaviour
             }
         }
 
-        //countdown from 100 enemies. @ 0 enemies, game over
-        if (score <= 0)
-        {
-            Debug.Log("Victory!");
-            PauseGame();
-            gameOver = true;
-        }
+
     }
 
-    public void Damage()
-    {
-        health = health - 2;
-        DrawHearts(health);
-    }
 
-    public void Score()
-    {
-        score--;
-        Debug.Log(score.ToString());
-    }
 
     //Swatter Logic for controls
     private void SwatterPressed()
@@ -96,57 +75,6 @@ public class PlayerControl : MonoBehaviour
         }
     }
 
-    void PauseGame()
-    {
-        Time.timeScale = 0;
-    }
 
-    private void DrawHearts(float currentHealth)
-    {
-        int heartPosDefaultX = -28;
-        int heartPosOffsetX = 5;
 
-        GameObject[] heartObjects;
-        heartObjects = GameObject.FindGameObjectsWithTag("Heart");
-
-        //destroy existing hearts for switch below
-        if (heartObjects != null)
-        {
-            foreach (GameObject heart in heartObjects)
-            {
-                Destroy(heart);
-            }
-        }
-
-        switch (currentHealth)
-        {
-            case 6:
-                Instantiate(fullHeartPrefab, new Vector3(heartPosDefaultX, 5, 21), fullHeartPrefab.transform.rotation);
-                Instantiate(fullHeartPrefab, new Vector3(heartPosDefaultX + (heartPosOffsetX), 5, 21), fullHeartPrefab.transform.rotation);
-                Instantiate(fullHeartPrefab, new Vector3(heartPosDefaultX + (heartPosOffsetX * 2), 5, 21), fullHeartPrefab.transform.rotation);
-                break;
-            case 5:
-                //something
-                break;
-            case 4:
-                Instantiate(fullHeartPrefab, new Vector3(heartPosDefaultX, 5, 21), fullHeartPrefab.transform.rotation);
-                Instantiate(fullHeartPrefab, new Vector3(heartPosDefaultX + (heartPosOffsetX), 5, 21), fullHeartPrefab.transform.rotation);
-                break;
-            case 3:
-                //something
-                break;
-            case 2:
-                Instantiate(fullHeartPrefab, new Vector3(heartPosDefaultX, 5, 21), fullHeartPrefab.transform.rotation);
-                break;
-            case 1:
-                //something
-                break;
-            default:
-                Debug.Log("Game Over");
-                gameOver = true;
-                PauseGame();
-                break;
-        }
-
-    }
 }
